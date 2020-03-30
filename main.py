4feb3f66f5c1e59f6ed7bfd7f413b2faf5756db2
kamiprok <kamiprok@gmail.com>
2020-03-30 19:09:23 +0200

    added pydantic basemodel

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
 
 @app.get('/')
 def root():
     return {"message": "Hello, World!"}
 
-@app.get('/hello/{name}')
+
+class HelloNameResp(BaseModel):
+    message: str
+
+
+@app.get('/hello/{name}', response_model=HelloNameResp)
 def hello_name(name: str):
-    return {"message": f"Hello {name}"}
+    return HelloNameResp(message=f"Hello {name}")
